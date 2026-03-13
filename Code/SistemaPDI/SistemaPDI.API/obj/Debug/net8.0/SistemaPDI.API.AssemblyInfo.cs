88ad9e13e38d0629f5d4c9974969b5cef2e8a8a2
gonca <gonca@GONCALO>
2026-03-13 09:53:25 +0000

    Adicionar Filtros à view dos lotes. Melhorar interface

--- Code/SistemaPDI/SistemaPDI.API/obj/Debug/net8.0/SistemaPDI.API.AssemblyInfo.cs
+++ Code/SistemaPDI/SistemaPDI.API/obj/Debug/net8.0/SistemaPDI.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaPDI.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+234bbe30d074cf9eb6276319778f813f0d6ee75e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53de54a44da7bcf3c9059ffe07111e23aafe35b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaPDI.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaPDI.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
